0322c283811b6ac0451daf50e6c1517ef017d9ab
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-29 14:44:32 +0200

    WIP implement FactParser

--- src/com/codecool/expert/FactParser.java
+++ src/com/codecool/expert/FactParser.java
@@ -1,5 +1,17 @@
 package com.codecool.expert;
 
 public class FactParser extends XMLParser {
-  public FactRepository getFactRepository() {}
+
+  public FactRepository fRepository;
+
+  public FactParser() {
+    this.fRepository = new FactRepository();
+  }
+
+  public FactRepository getFactRepository() {
+
+    FactRepository repository = new FactRepository();
+    loadXmlDocument("Fact.xml");
+    NodeList factList = doc.getElementByTagName("Fact");
+  }
 }
