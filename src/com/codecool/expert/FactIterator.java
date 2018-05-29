f0a11feff47253f14cdb9f3a6939c2728d98c5e6
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-29 14:44:00 +0200

    Implement FactIterator

---
+++ src/com/codecool/expert/FactIterator.java
@@ -0,0 +1,28 @@
+package com.codecool.expert;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FactIterator implements Iterator<Fact> {
+  public List<Fact> facts;
+  public int index;
+
+  public FactIterator(List<Fact> facts) {
+    this.facts = facts;
+    this.index = 0;
+  }
+
+  @Override
+  public boolean hasNext() {
+
+    if (this.facts.size() > index) return true;
+
+    this.index = 0;
+    return false;
+  }
+
+  @Override
+  public Fact next() {
+    return this.facts.get(index++);
+  }
+}
