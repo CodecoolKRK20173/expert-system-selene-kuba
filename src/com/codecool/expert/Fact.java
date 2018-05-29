36cc0b3776e8ad031998cfb2f2222cc14ac5fe04
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-29 14:43:31 +0200

    Implement Fact

--- src/com/codecool/expert/Fact.java
+++ src/com/codecool/expert/Fact.java
@@ -1,20 +1,33 @@
 package com.codecool.expert;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Fact {
-    
-    public Fact(String id, String description) {
+  String id;
+  String description;
+  Map<String, Boolean> valueId;
 
-    }
+  public Fact(String id, String description) {
+    this.id = id;
+    this.description = description;
+    this.valueId = new HashMap<>();
+  }
 
-    public Set<String> getIdSet() {
+  public Set<String> getIdSet() {
+    return this.valueId.keySet();
+  }
 
-    }
+  public void setFactValueById(String id, boolean value) {
+    this.valueId.set(id, value);
+  }
 
-    public void setFactValueById(String id) {
+  public boolean getValueById(String id) {
+    return this.valueId.get(id);
+  }
 
-    }
-
-    public String getDescription() {
-        
-    }
+  public String getDescription() {
+    return this.description;
+  }
 }
