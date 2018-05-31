4f57302a0d09336267a0f29a6dd975f8fc9ee2ff
SStreich <selene.streich@gmail.com>
2018-05-31 13:09:15 +0200

    move QuestionIterator to separate class and change it to public

--- src/com/codecool/expert/QuestionIterator.java
+++ src/com/codecool/expert/QuestionIterator.java
@@ -1,1 +1,29 @@
 package com.codecool.expert;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class QuestionIterator implements Iterator {
+    private List<Question> questions = new ArrayList<>();
+    private int index;
+
+    public QuestionIterator(List<Question> questions) {
+        this.index = 0;
+        this.questions = questions;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (questions.size() > index) return true;
+        return false;
+    }
+
+    @Override
+    public Question next() {
+        Question question = questions.get(index++);
+        //this.index = index++;
+        System.out.print(question);
+        return question;
+    }
+}
--- src/com/codecool/expert/RuleRepository.java
+++ src/com/codecool/expert/RuleRepository.java
@@ -1,6 +1,8 @@
 package com.codecool.expert;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -12,43 +14,16 @@ public class RuleRepository {
   public Map<String, Question> questionsMap = new HashMap<String, Question>();
 
   public RuleRepository() {
-    QuestionIterator questIterator = new QuestionIterator();
-    while (questIterator.hasNext()) {
-      addQuestion(questIterator.next());
-    }
   }
 
   public void addQuestion(Question question) {
     questionsMap.put(question.getId(), question);
   }
 
-    private class QuestionIterator implements Iterator {
-        private List<Question> questions = new ArrayList<>(questionsMap.values());
-        private int index;
-
-        public QuestionIterator() {
-            this.index = 0;
-            this.questions = questions;
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (questions.size() > index) return true;
-            return false;
-        }
-
-        @Override
-        public Question next() {
-            Question question = questions.get(index++);
-            //this.index = index++;
-            System.out.print(question);
-            return question;
-        }
-    }
+  public Iterator<Question> getIterator() {
+    List<Question> questions =  new ArrayList<Question>(questionsMap.values());
+    return new QuestionIterator(questions);
   }
-
-  // public Iterator<Question> getIterator() {
-
-  // }
+}
 
 
