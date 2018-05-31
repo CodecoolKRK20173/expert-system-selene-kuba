08d720c847a14ff1ce96da7b3a67fc666b137ff4
SStreich <selene.streich@gmail.com>
2018-05-31 11:14:32 +0200

    implement SingleValue() constructor and SingleValue fields

--- src/com/codecool/expert/SingleValue.java
+++ src/com/codecool/expert/SingleValue.java
@@ -1,8 +1,11 @@
 package com.codecool.expert;
 
 public class SingleValue extends Value {
+    private String param;
+    private boolean selectionType;
 
     public SingleValue(String param, boolean selectionType) {
-        
+        this.param = param;
+        this.selectionType = selectionType;
     }
 }
