29f3d75aec0c39fa23b915be2d065b799c46cd04
SStreich <selene.streich@gmail.com>
2018-05-31 11:07:37 +0200

    modify main for testing RuleParser class

--- src/com/codecool/expert/Main.java
+++ src/com/codecool/expert/Main.java
@@ -6,16 +6,17 @@ import java.util.Map.*;
 public class Main {
 
   public static void main(String[] args) {
+    RuleParser rParser = new RuleParser();    
     FactRepository factR = new FactRepository();
     FactParser factP = new FactParser();
     factR = factP.getFactRepository();
     Iterator<Fact> iterF = factR.getIterator();
-    while (iterF.hasNext()) {
-      Fact fact = iterF.next();
-      System.out.println(fact.id);
-      System.out.println(fact.getDescription());
-      System.out.println(fact.getIdSet());
-      System.out.println(fact.getValueSet());
-    }
+    //while (iterF.hasNext()) {
+      //Fact fact = iterF.next();
+      //System.out.println(fact.id);
+      //System.out.println(fact.getDescription());
+      //System.out.println(fact.getIdSet());
+      //System.out.println(fact.getValueSet());
+    //}
   }
 }
