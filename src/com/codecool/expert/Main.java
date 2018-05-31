b986f6a2eb18ee095e0b712c902558e55d7d720f
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-05-31 12:42:43 +0200

    Apply clean code

--- src/com/codecool/expert/Main.java
+++ src/com/codecool/expert/Main.java
@@ -1,14 +1,12 @@
 package com.codecool.expert;
 
 import java.util.Iterator;
-import java.util.Map.*;
 
 public class Main {
 
   public static void main(String[] args) {
-    FactRepository factR = new FactRepository();
     FactParser factP = new FactParser();
-    factR = factP.getFactRepository();
+   FactRepository factR = factP.getFactRepository();
     Iterator<Fact> iterF = factR.getIterator();
     while (iterF.hasNext()) {
       Fact fact = iterF.next();
