fb0b7f6542bcf7057cc645b1a6e0e5acc6f23efb
SStreich <selene.streich@gmail.com>
2018-05-31 11:13:59 +0200

    fix RuleParser(), add createRuleRepository() ~ add also methods addValueToAnswer(), addSingleValue(), addMultiValue for clean code ~ all of them are used to parse xml and create new objects for RuleRepository

--- src/com/codecool/expert/RuleParser.java
+++ src/com/codecool/expert/RuleParser.java
@@ -1,12 +1,16 @@
 package com.codecool.expert;
 
-import org.w3c.dom.NodeList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.w3c.dom.*;
 
 public class RuleParser extends XMLParser {
 
     private RuleRepository ruleRepo;
     public NodeList ruleList;
+
     
     public RuleRepository getRuleRepository() {
         return this.ruleRepo;
@@ -15,30 +19,49 @@ public class RuleParser extends XMLParser {
     public RuleParser() {
         loadXmlDocument("src/com/codecool/expert/Rules.xml");
         this.ruleList = doc.getElementsByTagName("Rule");
+        this.ruleRepo = new RuleRepository();
+        createRuleRepository();
+    }
+    private void createRuleRepository() {
         for (int i = 0; i < ruleList.getLength(); i++) {
             Element rule = (Element) ruleList.item(i);
             String id = rule.getAttribute("id");            
-            NodeList questions = rule.getChildNodes();
-            for (int j = 0; j < questions.getLength(); j++) {
-                Node quest = questions.item(j);
-                if ( quest.getNodeName().equals("Question")) {
-                    String question = quest.getTextContent();
-                    System.out.println(question);
-                }
-                if (quest.getNodeName().equals("MultipleValue")) {
-                    System.out.print("DDDD");
-                }
-                
-            }
-            // Element an = (Element) question.item(3).getChildNodes().item(3);
-            // System.out.println(an.getAttribute("value"));
-            //String question = rule.getTextContent();
-            //String question = rule.getTextContent();
-            //int answer = rule.getChildNodes().getLength();
-
-            // System.out.print(rule+""+question);
-            System.out.println("-------");
+            Element elementQuest = (Element) rule.getElementsByTagName("Question").item(0);
+            String quest = elementQuest.getTextContent();
+            NodeList selectionList = rule.getElementsByTagName("Selection");
+            Answer answer = new Answer();
+            addValuesTo(answer, selectionList);
+            Question question = new Question(id, quest, answer);
+            ruleRepo.addQuestion(question);
         }
-        //this.ruleRepo = getRuleRepository();
+    }
+
+    private void addValuesTo(Answer answer, NodeList selectionList) {
+        for (int j = 0; j < selectionList.getLength(); j++) {
+            Value value;
+            Element selection = (Element) selectionList.item(j);                 
+            Boolean answerType = selection.getAttribute("value").equals("true"); 
+            NodeList values = selection.getElementsByTagName("SingleValue");
+            value = isEmpty(values) ? addMultiValue(answerType, selection) : addSingleValue(answerType, values);
+            answer.addValue(value);
+        }
+    }
+
+    private Value addSingleValue(Boolean answerType, NodeList values) {
+        String param = values.item(0).getAttributes().item(0).getNodeValue();
+        SingleValue singleValue = new SingleValue(param, answerType);
+        return (Value) singleValue;
+    }
+
+    private Value addMultiValue(Boolean answerType, Element selection) {
+        Node params = selection.getElementsByTagName("MultipleValue").item(0).getAttributes().item(0);
+        List<String> paramsInList = Arrays.asList(params.getNodeValue().split(","));
+        MultipleValue multiValue = new MultipleValue(paramsInList, answerType);
+        return (Value) multiValue;
+    }
+
+    private Boolean isEmpty(NodeList values) {
+        if (values.getLength() > 0) return false;
+        return true;
     }
 }
